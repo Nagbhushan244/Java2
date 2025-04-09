fa5f97ebd71fdda6e89eea7500e2501ce4a37042
Nagbhushan244 <01fe23bca244@kletech.ac.in>
2025-04-09 15:25:37 +0530

    Save Java File with changes

--- test.java
+++ test.java
@@ -3,6 +3,7 @@ package testjava;
 public class test {
     public static void main(String[] args) {
         System.out.println("This is testing java file on git(main)");
+        System.out.println("This is testing java file on git(master)");
     }
 
 }
